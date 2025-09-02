208fbd732b23e44a6ab44a269f533c0a370be9eb
leogiroux <leogiroux.pro@gmail.com>
2025-09-02 16:44:33 +0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "GimiiSDK",
+  products: [
+    .library(name: "GimiiSDK", targets: ["GimiiSDK"]),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "GimiiSDK",
+      url: "https://github.com/Gimii-solutions/gimii-ios-sdk/releases/download/1.0.0/GimiiSDK.xcframework.zip",
+      checksum: "<actual_checksum_from_workflow>"
+    )
+  ]
+)
